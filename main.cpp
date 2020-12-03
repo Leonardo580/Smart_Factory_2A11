2ab4ac4b84bb67695ebe2b606cc07176ae095a43
aziz-scofield <61650760+aziz-scofield@users.noreply.github.com>
2020-12-03 22:24:10 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
 
+#include <QCoreApplication>
+#include <QtSql>
+#include <QDebug>
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.setWindowIcon(QIcon("C:\\Users\\Anas\\OneDrive\\Workload\\Project 2\\logo.JPG"));
-  w.setFont(QFont("Agency FB"));
-  w.show();
+    QApplication a(argc, argv);
+    connection c;
+    bool test=c.createconnect();
+    MainWindow w;
 
-  return a.exec();
+    if(test)
+    {
+    w.show();
+    QMessageBox::information(nullptr,QObject::tr("database is open"),QObject::tr("connection successful.\n" "Click Cancel to exit. "),QMessageBox::Cancel);
+    QMessageBox::critical(nullptr,QObject::tr("database is open"),QObject::tr("connection successful.\n" "Click Cancel to exit. "),QMessageBox::Cancel);
+
+    }
+    else
+        QMessageBox::critical(nullptr,QObject::tr("database is open"),QObject::tr("connection successful.\n" "Click Cancel to exit. "),QMessageBox::Cancel);
+
+    return a.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,56 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <
-QT_BEGIN_NAMESPACE
+#include "Publicite.h"
+#include"Promo.h"
+#include "QMediaPlayer"
+#include "QMediaPlaylist"
+
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+     void click();
+
+private slots:
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+    void on_pb_modifier_clicked();
+
+
+    void on_pr_ajouter_clicked();
+
+    void on_pr_supprimer_clicked();
+
+
+
+    void on_tri_1_activated(const QString &arg1);
+
+
+
+    void on_pushButton_clicked();
+
+
+
+    void on_cher_cin_cursorPositionChanged(int arg1, int arg2);
+
+    void on_cher_cin_2_cursorPositionChanged(int arg1, int arg2);
+
 
 private:
-  Ui::MainWindow *ui;
+    Ui::MainWindow *ui;
+    Publicite pub;
+    Promo pr;
+    QMediaPlaylist* music1;
+    QMediaPlayer* music;
 };
+
 #endif // MAINWINDOW_H
