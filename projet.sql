18c576c3548e6eb2c82b3a2ca166f6c2b3ddbc14
Leonardo580 <anasbenbrahim9@gmail.com>
2021-01-01 21:14:07 +0100

    second

---
+++ projet.sql
@@ -0,0 +1,22 @@
+/*create table client (CIN number constraint pk_CIN Primary Key,nom varchar2(30),
+                     prenom varchar2(30),numero varchar(8),adresse varchar(100),
+                     date_naissance varchar(10)); */
+/*create table commande(CINC number,type varchar2(30),
+                      adresse varchar2(100),date_paiment varchar(10),quantite number,prixunitaire number
+                     ,prix number);*/
+/* create TABLE Employees (
+     CIN VARCHAR2(8) primary key, 
+     Full_Name VARCHAR2(30), 
+    Age NUMBER,
+     Address VARCHAR2(50), 
+     Email VARCHAR2(50)
+); 
+
+ create TABLE Posts(
+  id NUMBER  constraint pk_id primary key, 
+   CIN VARCHAR2(8) constraint fk_cin REFERENCES Employees(CIN),
+  Job_Desc VARCHAR2(50),
+  Salary NUMBER, 
+  Benefits NUMBER, 
+  Hours_worked NUMBER
+  ); */
